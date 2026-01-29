b362434955e1d4bcc06a309fe5a28fbf7fd1dc25
SotonyeJoe <sotonye28@gmail.com>
2026-01-29 15:14:12 +0100

    worked on d user datatable

--- react-admin-ui/src/pages/home/Home.tsx
+++ react-admin-ui/src/pages/home/Home.tsx
@@ -11,6 +11,7 @@ import {
 } from "../../components/data";
 import BarChartBox from "../../components/barChartBox/BarChartBox";
 import PieChartBox from "../../components/pieChartBox/PieChartBox";
+import BigChartBox from "../../components/bigChartBox/BigChartBox";
 
 const Home = () => {
   return (
@@ -23,7 +24,7 @@ const Home = () => {
       <div className="box box4"><PieChartBox /></div>
       <div className="box box5"><ChartBox  {...chartBoxConversion}/></div>
       <div className="box box6"><ChartBox  {...chartBoxRevenue}/></div>
-      <div className="box box7">Box7</div>
+      <div className="box box7"><BigChartBox /></div>
       <div className="box box8"><BarChartBox  {...barChartBoxVisit} /></div>
       <div className="box box9"><BarChartBox  {...barChartBoxRevenue} /></div>
       <div className="box box10">Box10</div>
